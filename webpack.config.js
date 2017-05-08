5b6a626e42f2b44e4ff8fe79f2cdc752356ce516
Sam Meyer <sam@credsimple.com>
2017-05-07 20:48:35 -0400

    Fix font paths

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 const fontPath = path.join(__dirname, 'src/fonts')
 const srcPath = path.join(__dirname, 'src')
 const appPath = path.join(__dirname, 'dist/')
-const stylesPath = path.join(__dirname, 'src/styles')
+// const stylesPath = path.join(__dirname, 'src/styles')
 const imgPath = path.join(__dirname, 'src/img')
 
 var config = {
@@ -14,7 +14,7 @@ var config = {
   output: {
     path: appPath,
     filename: 'bundle.js',
-    publicPath: appPath
+    publicPath: 'dist/'
   },
   module: {
     loaders: [
@@ -23,21 +23,20 @@ var config = {
         exclude: /node_modules/,
         loaders: ['babel-loader'],
       },
-      { include: stylesPath,
+      {
         test: /\.css$/,
         loader: 'style-loader'
       },
       {
-        include: stylesPath,
         test: /\.css$/,
         loader: 'css-loader',
       },
-      { include: fontPath, test: /\.svg$/, loader: 'url?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]' },
-      { include: fontPath, test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
-      { include: fontPath, test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]' },
-      { include: fontPath, test: /\.ttf$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
-      { include: fontPath, test: /\.[ot]tf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
-      { include: fontPath, test: /\.eot$/, loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]' },
+      { test: /\.svg$/, loader: 'url?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]' },
+      { test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
+      { test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]' },
+      { test: /\.ttf$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
+      { test: /\.[ot]tf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
+      { test: /\.eot$/, loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]' },
       { test: /\.(png|jpg)$/, loader: 'url-loader' }
     ],
   },
